91f9622d3cf5c030bd62f4a4f995e7d6e7983bdf
ChinnYuu <jasonboy0526@gmail.com>
2019-11-05 17:06:55 +0800

    tx_declarative

--- src/main/java/com/cyl/spring/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/cyl/spring/service/impl/TeacherServiceImpl.java
@@ -17,29 +17,29 @@ import org.springframework.transaction.support.TransactionTemplate;
 @Component
 public class TeacherServiceImpl implements ITeacherService {
 
-    @Autowired
-    private TransactionTemplate transactionTemplate;
+//    @Autowired
+//    private TransactionTemplate transactionTemplate;
 
     @Autowired
     private ITeacherDao teacherDao;
 
     public void add() {
-        transactionTemplate.execute(new TransactionCallback<Object>() {
-
-            public Object doInTransaction(TransactionStatus transactionStatus) {
+//        transactionTemplate.execute(new TransactionCallback<Object>() {
+//
+//            public Object doInTransaction(TransactionStatus transactionStatus) {
                 Teacher t1 = new Teacher();
                 t1.setName("applewww");
                 teacherDao.add(t1);
 
                 //中間發生異常
-                int i =1/0;
+//                int i =1/0;
                 Teacher t2 = new Teacher();
                 t2.setName("apple2ww");
                 teacherDao.add(t2);
                 teacherDao.add(t2);
-                return null;
+//                return null;
             }
-        });
-
-    }
+//        });
+//
+//    }
 }
